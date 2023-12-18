72c16e6cec5b9035e084c50a1d8f546e127d5053
soaresfa <fbsoares341@gmail.com>
2023-12-18 12:55:57 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { GridComponent } from './modules/tipoveiculo/grid/grid.component';
+
+export const routes: Routes = [
+    { path: 'grid-tipoveiculo', component: GridComponent }
+];
